191900d2a4078676d3bd97c2abe61e3665d81a2f
MONISH-VELUSAMY <monish.vgp@gmail.com>
2021-08-21 15:51:10 +0530

    reactjs

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import {Para} from './Para';
+function Header(){
+
+
+    return(
+    
+      <div>
+        <h1>This is header component</h1>
+        <h2>Hello I am Monish!!! </h2>
+      <Para/>
+      </div>
+         
+    
+    
+    );
+    
+    
+    
+    
+}
+ export default Header;
---
+++ src/Para.js
@@ -0,0 +1,9 @@
+import React from "react";
+export class Para extends React.Component{
+    render(){
+        return(
+           <div><h3>This is paragraph component</h3><p>Hello I am Monish.I am pursuing my engineering degree in b.tech. 
+            IT at irtt erode.</p><br/></div>
+            );
+    }
+}
